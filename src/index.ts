802827ee1ebf63ba7ce2548613ad8f0c8be3b8b1
İslam Uyanık <islamuyanik@gmail.com>
2022-11-21 01:09:45 +0300

    type,interface,@types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello type");
+import express from 'express'
+import { type } from 'os';
+
+const app=express();
+
+
+interface Params{
+    a:number;
+    b:number;
+}
+
+type Add = (x: Params) => number;
+
+const add: Add= x => {
+    return x.a+x.b;
+};
+
+app.get('/', req=>{
+   add({a:1, b:2})
+})
+
+app.listen(3001, () =>{
+    console.log('started')
+})
